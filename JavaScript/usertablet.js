41cdc2884a56515920e9bf9b8eed27fb3d12057b
NinjaAnge <aleskj91@live.no>
2018-04-24 14:22:33 +0200

    dasdasdasd

---
+++ JavaScript/usertablet.js
@@ -0,0 +1,95 @@
+var listYo = [{  
+    "id":1,
+    "first_name":"Dawna",
+    "last_name":"Willerton",
+    "email":"dwillerton0@bbb.org",
+    "gender":"Female",
+    "adresse":"250 Knutson Parkway"
+ },
+ {  
+    "id":2,
+    "first_name":"Neilla",
+    "last_name":"Ashpole",
+    "email":"nashpole1@examiner.com",
+    "gender":"Female",
+    "adresse":"95944 Jana Plaza"
+ },
+ {  
+    "id":3,
+    "first_name":"Gino",
+    "last_name":"Eastcott",
+    "email":"geastcott2@naver.com",
+    "gender":"Male",
+    "adresse":"08 Miller Center"
+ },
+ {  
+    "id":4,
+    "first_name":"Carina",
+    "last_name":"Poole",
+    "email":"cpoole3@linkedin.com",
+    "gender":"Female",
+    "adresse":"87321 Prairieview Crossing"
+ },
+ {  
+    "id":5,
+    "first_name":"Nerissa",
+    "last_name":"Paskins",
+    "email":"npaskins4@qq.com",
+    "gender":"Female",
+    "adresse":"4980 Nevada Hill"
+ },
+ {  
+    "id":6,
+    "first_name":"Bronny",
+    "last_name":"Dumingo",
+    "email":"bdumingo5@goo.ne.jp",
+    "gender":"Male",
+    "adresse":"4277 Meadow Valley Street"
+ },
+ {  
+    "id":7,
+    "first_name":"Delora",
+    "last_name":"Matiebe",
+    "email":"dmatiebe6@mashable.com",
+    "gender":"Female",
+    "adresse":"25 Village Terrace"
+ },
+ {  
+    "id":8,
+    "first_name":"Elnore",
+    "last_name":"Btham",
+    "email":"ebtham7@hostgator.com",
+    "gender":"Female",
+    "adresse":"8 Haas Center"
+ },
+ {  
+    "id":9,
+    "first_name":"Loralyn",
+    "last_name":"Revely",
+    "email":"lrevely8@rambler.ru",
+    "gender":"Female",
+    "adresse":"3826 Bashford Alley"
+ },
+ {  
+    "id":10,
+    "first_name":"Marrissa",
+    "last_name":"Poltone",
+    "email":"mpoltone9@bandcamp.com",
+    "gender":"Female",
+    "adresse":"845 Corry Center"
+ }]
+
+
+
+ function findObjectByKey(array, key, value) {
+     for (var i= 0; i < array.length; i++) {
+         if (array [i] [key] === value) {
+             return array[i];
+            }
+         }
+         return null;
+ }
+ var obj = findObjectByKey(listYo, 'id', 1);
+ console.log();
+
+ ocument.querySelector('#2').innerHTML =(ul)
